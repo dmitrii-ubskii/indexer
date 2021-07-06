844eb1478ddbe3812e50ab57bf87f6c393365fd1
Dmitrii Ubskii <ubskydm@gmail.com>
2021-07-06 18:32:21 +0300

    Basic REPL

--- src/repl.cpp
+++ src/repl.cpp
@@ -1,1 +1,15 @@
-int main(){return 0;}
+#include <iostream>
+#include <string>
+
+int main()
+{
+	std::string cmd;
+	std::cout << "Type \"help\" or \"?\" for help, \"quit\" to quit\n";
+	do {
+		std::cout << ">>> ";
+		std::getline(std::cin, cmd);
+		std::cout << cmd << "\n";
+	} while (cmd != "q" && not std::cin.eof());
+
+	return 0;
+}
