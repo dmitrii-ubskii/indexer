1fe797b8372134ff84686dbf19c4cf97bc2ef806
Dmitrii Ubskii <ubskydm@gmail.com>
2021-07-14 14:32:03 +0300

    WIP: Windows FS watcher threads

--- src/windows_filesystem_watcher.cpp
+++ src/windows_filesystem_watcher.cpp
@@ -1,13 +1,9 @@
 #include "indexer/filesystem_watcher.h"
 
-#include <cassert>
-#include <stdexcept>
-#include <unordered_map>
+#include <mutex>
+#include <thread>
 
-#include <poll.h>
-#include <sys/inotify.h>
-#include <sys/ioctl.h>
-#include <unistd.h>
+#include <Windows.h>
 
 #include "indexer/path_utils.h"
 
@@ -22,21 +18,66 @@ public:
 
 	void addFile(std::filesystem::path const& path)
 	{
+		constexpr auto flags = FILE_NOTIFY_CHANGE_DIR_NAME | FILE_NOTIFY_CHANGE_FILE_NAME | FILE_NOTIFY_CHANGE_ATTRIBUTES |
+			FILE_NOTIFY_CHANGE_SIZE | FILE_NOTIFY_CHANGE_LAST_WRITE | FILE_NOTIFY_CHANGE_SECURITY;
+		watchThreads.insert({path, PathWatchThread{path, flags, &eventQueue}});
 	}
 
 	void addDirectory(std::filesystem::path const& path)
 	{
+		constexpr auto flags = FILE_NOTIFY_CHANGE_DIR_NAME | FILE_NOTIFY_CHANGE_ATTRIBUTES | FILE_NOTIFY_CHANGE_FILE_NAME;
+		watchThreads.insert({path, PathWatchThread{path, flags, &eventQueue}});
 	}
 
 	void removePath(std::filesystem::path const& path)
 	{
+		if (watchThreads.contains(path))
+		{
+			watchThreads.erase(path);
+		}
 	}
 
 	std::vector<FilesystemWatcher::Event> pollEvents()
 	{
+		return {};
 	}
 
 private:
+	class EventQueue
+	{
+
+	} eventQueue;
+
+	class PathWatchThread
+	{
+	public:
+		PathWatchThread(std::filesystem::path const& path, unsigned flags, EventQueue* eventQueue)
+			: thread{&PathWatchThread::watchLoop, this, path, flags, eventQueue}
+		{}
+
+		PathWatchThread(PathWatchThread&&) = default;
+		PathWatchThread& operator=(PathWatchThread&&) = default;
+
+		~PathWatchThread()
+		{
+			doStop = true;
+			thread.join();
+		}
+
+	private:
+		void watchLoop(std::filesystem::path path, unsigned flags, EventQueue* eventQueue)
+		{
+			while (not doStop)
+			{
+
+			}
+		}
+
+		std::thread thread;
+		bool doStop{false};
+	};
+
+	std::unordered_map<std::filesystem::path, PathWatchThread, PathHasher> watchThreads;
 };
 
 FilesystemWatcher::FilesystemWatcher(): pImpl{std::make_unique<FilesystemWatcherImpl>()} {}
